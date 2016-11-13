0884b833a048067026cc7e43d65cf8707da0aa48
Avik Basu <avikbasu93@gmail.com>
2016-11-12 16:31:54 -0800

    Added majority voting using probability functionality

--- src/classifier.py
+++ src/classifier.py
@@ -6,6 +6,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.externals import six
 from sklearn.pipeline import _name_estimators
 from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
 
 class MetaClassifier(BaseEstimator, ClassifierMixin):
     """ A combined multi-class classifier classifier
@@ -29,7 +30,7 @@ class MetaClassifier(BaseEstimator, ClassifierMixin):
     """
 
     def __init__(self, classifiers, vote='probability',
-                weights='None', method='majority_voting'):
+                weights=None, method='majority_voting'):
         self.classifiers = classifiers
         self.named_classifiers = {k:v for k,v in _name_estimators(classifiers)}
         self.vote = vote
@@ -58,47 +59,58 @@ class MetaClassifier(BaseEstimator, ClassifierMixin):
             raise TypeError
             sys.exit()
         self.lablenc_ = LabelEncoder()
+        X_list = map(np.vstack, X_list)
+        y_list = map(np.hstack, y_list)
         self.lablenc_.fit(y_list[0]) # make sure both y vectors have both the classes
         self.classes_ = self.lablenc_.classes_
-        X_list = map(np.vstack, X_list)
-        y_list = map(np.stack, y_list)
         self.classifiers_ = []
         for i,clf in enumerate(self.classifiers):
             fitted_clf = clone(clf).fit(X_list[i],
                                 self.lablenc_.transform(y_list[i]))
             self.classifiers_.append(fitted_clf)
-        print self
         return self
 
     def predict(self, X_list):
         num_clfs = len(self.classifiers_)
-        print len(self.classifiers_)
-        #print self.classifiers_[1].predict_proba(X_list[1])
-        #raw_input()
-        if self.vote == 'probability':
-            probas = np.asarray([clf.predict_proba(X_list[i])
-                                for i,clf in enumerate(self.classifiers_)])
+        preds = []
+        for index, X in enumerate(X_list):
+            pred = [np.mean(self.classifiers_[index].predict_proba(P), axis=0) for P in X]
+            preds.append(pred)
+        preds = np.asarray(preds)
+        print preds
+        weighted_proba = np.average(preds, axis=0, weights=self.weights) 
+        print weighted_proba
+        maj_vote = np.argmax(weighted_proba, axis=1)
+        return maj_vote
+        
             
 
+# Just for debugging and testing
 
-x1 = np.array([[1,2], [4,1], [3,1]])
-x2 = np.array([[6,4],[8,9]])
-y1 = np.array([0,1,1])
-y2 = np.array([1,0])
+x1 = np.array([ np.array([[1,5,7], [1,2,4], [1,8,9]]), 
+                np.array([[2,8,6], [2,0,3]]), 
+                np.array([[3,7,5], [3,4,3], [3,9,7]]) 
+                ])
+
+x2 = np.array([ np.array([[1,5,7], [1,2,4]]), 
+                np.array([[2,8,6], [2,0,3], [2,5,5]]), 
+                np.array([[3,7,5], [3,4,3], [3,9,7]])
+                ])
+
+y1 = np.array([ np.array([1,1,1]),
+                np.array([1,1]),
+                np.array([0,0,0])
+                ])
+
+y2 = np.array([ np.array([0,0]), 
+                np.array([0,0,0]), 
+                np.array([1,1,1])
+                ])
 
 X = [x1,x2]
 y = [y1,y2]
 
-x1 = np.array([ np.array([[1,5,7], [1,2,4], [1,8,9]]), 
-                np.array([[2,8,6], [2,0,3]]), 
-                np.array([[3,7,5], [3,4,3], [3,9,7]]) ])
-
-x2 = np.array([np.array([[1,5,7], [1,2,4]]), 
-            np.array([[2,8,6], [2,0,3], [2,5,5]]), 
-            np.array([[3,7,5], [3,4,3], [3,9,7]])])
-
-
-clfs = [SVC(probability=True), SVC(probability=True)]
+clfs = [LogisticRegression(C=100, penalty='l2'), LogisticRegression(C=10,penalty='l1')]
 meta_clf = MetaClassifier(clfs)
 meta_clf.fit(X,y)
-#meta_clf.predict(X)
+print meta_clf.predict(X)
