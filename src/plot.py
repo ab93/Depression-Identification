85b6e3381c1d2dd05fecf9ef89bf7fd2c4dfb882
ab93 <avikbasu93@gmail.com>
2016-11-17 20:14:23 -0800

    Added CLM files

--- src/plot.py
+++ src/plot.py
@@ -6,15 +6,17 @@ import matplotlib.pyplot as plt
 import config
 
 def get_combined_data(file1, file2):
-    feature_df = pd.read_csv(file1, header=None)
+    feature_df = pd.read_csv(file1)
+    #print feature_df
+    #df = pd.read_csv(file2)
+    #print feature_df
     feature_df = feature_df.append(pd.read_csv(file2, header=None))
-
+    
     train_split_df = pd.read_csv(config.TRAIN_SPLIT_FILE, 
                     usecols=['Participant_ID', 'PHQ_Binary'])
-    feature_df = feature_df[feature_df[0].isin(train_split_df['Participant_ID'])]
-    
-
+    feature_df = feature_df[feature_df['video'].isin(train_split_df['Participant_ID'])]
     print feature_df
+
 
 def main():
     get_combined_data(os.path.join('../data', sys.argv[1]), 
