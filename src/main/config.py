518cd2ea6536057bf211abbcc90c57a255bc11de
ab93 <avikbasu93@gmail.com>
2016-11-26 17:03:47 -0800

    Changed file path to be platform independent

--- src/main/config.py
+++ src/main/config.py
@@ -1,5 +1,13 @@
-TRAIN_SPLIT_FILE = 'data/classification_data/training_split.csv'
-TEST_SPLIT_FILE = 'data/classification_data/test_split.csv'
-VAL_SPLIT_FILE = 'data/classification_data/dev_split.csv'
-D_ND_DIR = 'data/disc_nondisc'
-POS_NEG_DIR = 'data/pos_neg'
+import os
+
+TRAIN_SPLIT_FILE = os.path.join('data','classification_data','training_split.csv')
+TEST_SPLIT_FILE = os.path.join('data','classification_data','test_split.csv')
+VAL_SPLIT_FILE = os.path.join('data','classification_data','dev_split.csv')
+D_ND_DIR = os.path.join('data','disc_nondisc')
+POS_NEG_DIR = os.path.join('data','pos_neg')
+
+# TRAIN_SPLIT_FILE = 'data/classification_data/training_split.csv'
+# TEST_SPLIT_FILE = 'data/classification_data/test_split.csv'
+# VAL_SPLIT_FILE = 'data/classification_data/dev_split.csv'
+# D_ND_DIR = 'data/disc_nondisc'
+# POS_NEG_DIR = 'data/pos_neg'
