aae8e13033eabbc695a686575d41fbc9656994e6
ab93 <avikbasu93@gmail.com>
2016-11-27 18:55:26 -0800

    Added anova and grid_search dir paths

--- src/main/config.py
+++ src/main/config.py
@@ -6,6 +6,9 @@ VAL_SPLIT_FILE = os.path.join('data','classification_data','dev_split.csv')
 D_ND_DIR = os.path.join('data','disc_nondisc')
 POS_NEG_DIR = os.path.join('data','pos_neg')
 SEL_FEAT = os.path.join('data','selected_features')
+ANOVA_DIR = os.path.join('results','anova')
+GRID_SEARCH_DIR = os.path.join('results','grid_search')
+
 
 
 # TRAIN_SPLIT_FILE = 'data/classification_data/training_split.csv'
