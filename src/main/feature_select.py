edc1e602a917194ba7eda2d4ac5744aaad986981
AparnaAnand <aparnaan@usc.edu>
2016-11-29 18:45:24 -0800

    Automated some code and modified output

--- src/main/feature_select.py
+++ src/main/feature_select.py
@@ -33,9 +33,9 @@ def get_feature_df(file_, *files):
         labels[i] = train_split_dict[video_id]['PHQ_Binary']
     feature_df['label'] = pd.Series(labels, index=feature_df.index)
     try:
-        feature_df.drop(['video','question','starttime','endtime'], inplace=True, axis=1)
+        feature_df.drop(['question','starttime','endtime'], inplace=True, axis=1)
     except ValueError:
-        feature_df.drop(['video','question'], inplace=True, axis=1)
+        feature_df.drop(['question'], inplace=True, axis=1)
     return feature_df
 
 def performPCA(df):
@@ -46,27 +46,27 @@ def performPCA(df):
     pca.fit(X)
     return pca.components_
 
-def removeLowVariance(df):
-    #df1 = df.drop(['frame', 'timestamp','confidence','success','label'], axis=1)
-    df1 = df.drop(['label'], axis=1)
+def removeLowVariance(mode,df):
+    if mode=="V":
+        df1 = df.drop(['video','frame', 'timestamp','confidence','success','label'], axis=1)
+    else:
+        df1 = df.drop(['video','label'], axis=1)
     column_names=list(df1.columns.values)
     X=df1.as_matrix()
-    sel = VarianceThreshold(0.5)
+    sel = VarianceThreshold(0.95)
     sel.fit(X)
     selected_feature_idxs = sel.get_support(indices=True)
     selected_features = [column_names[i] for i in selected_feature_idxs]
     print selected_features
-    #final_selection = ['frame', 'timestamp','confidence','success']
-    final_selection = []
+    final_selection = ['video']
     final_selection.extend(selected_features)
     final_selection.extend(['label'])
     print "First Level: ",len(final_selection)
     final_df = df[final_selection]
     return final_df
 
-def performL1(df):
-    #df1 = df.drop(['frame', 'timestamp','confidence','success','label'], axis=1)
-    df1 = df.drop(['label'], axis=1)
+def performL1(mode,df):
+    df1 = df.drop(['video','label'], axis=1)
     column_names=list(df1.columns.values)
     X = df1.as_matrix()
     y = df['label'].values
@@ -74,17 +74,15 @@ def performL1(df):
     model = SelectFromModel(svc,prefit=True)
     selected_feature_idxs = model.get_support(indices=True)
     selected_features = [column_names[i] for i in selected_feature_idxs]
-    #final_selection = ['frame', 'timestamp','confidence','success']
-    final_selection = []
+    final_selection = ['video']
     final_selection.extend(selected_features)
     final_selection.extend(['label'])
     final_df = df[final_selection]
     print "Second Level: ",len(final_selection)
     return final_df
 
-def selectBestK(df):
-    #df1 = df.drop(['frame', 'timestamp','confidence','success','label'], axis=1)
-    df1 = df.drop(['label'], axis=1)
+def selectBestK(mode,df):
+    df1 = df.drop(['video','label'], axis=1)
     column_names=list(df1.columns.values)
     X = df1.as_matrix()
     y = df['label'].values
@@ -93,17 +91,15 @@ def selectBestK(df):
     selected_feature_idxs = kbest.get_support(indices=True)
     selected_features = [column_names[i] for i in selected_feature_idxs]
     print selected_features
-    #final_selection = ['frame', 'timestamp','confidence','success']
-    final_selection = []
+    final_selection = ['video']
     final_selection.extend(selected_features)
     final_selection.extend(['label'])
     final_df = df[final_selection]
     print "Fourth Level: ",len(final_selection)
     return final_df
 
-def performRandomForest(df):
-    #df1 = df.drop(['frame', 'timestamp', 'confidence', 'success', 'label'], axis=1)
-    df1 = df.drop(['label'], axis=1)
+def performRandomForest(mode,df):
+    df1 = df.drop(['video','label'], axis=1)
     column_names=list(df1.columns.values)
     X = df1.as_matrix()
     y = df['label'].values
@@ -119,11 +115,16 @@ def performRandomForest(df):
     selected_feature_idxs = []
     for a, b in sorted_pairs:
         selected_feature_idxs.append(b)
-    selected_feature_idxs = selected_feature_idxs[:50]
+    if mode=="V":
+        till = 100
+    elif mode=="A":
+        till = 20
+    else:
+        till = 50
+    selected_feature_idxs = selected_feature_idxs[:till]
     selected_features = [column_names[i] for i in selected_feature_idxs]
     print selected_features
-    #final_selection = ['frame', 'timestamp','confidence','success']
-    final_selection = []
+    final_selection = ['video']
     final_selection.extend(selected_features)
     final_selection.extend(['label'])
     final_df = df[final_selection]
@@ -131,17 +132,41 @@ def performRandomForest(df):
     return final_df
 
 def main():
-    file1 = os.path.join(config.D_ND_DIR,sys.argv[1])
-    files = [os.path.join(config.D_ND_DIR,argv) for argv in sys.argv[2:]]
+    qtype = sys.argv[1]
+    mode = sys.argv[2]
+    if mode == "V":
+        file_list = ["discriminative_CLM.csv","discriminative_CLM_3D.csv","discriminative_CLM_Gaze.csv","discriminative_CLM_pose.csv","discriminative_FACET.csv"]
+    elif mode == "A":
+        file_list = ["discriminative_COVAREP.csv","discriminative_FORMANT.csv"]
+    else:
+        file_list = ["discriminative_LIWC.csv"]
+    if qtype == "N":
+        for i in range(len(file_list)):
+            file_list[i] = "non"+file_list[i]
+    file1 = os.path.join(config.D_ND_DIR,file_list[0])
+    files = [os.path.join(config.D_ND_DIR,arg) for arg in file_list[1:]]
     df = get_feature_df(file1, files)
     print df
     #performPCA(df)
-    #df = removeLowVariance(df)
-    df = performL1(df)
-    df = performRandomForest(df)
-    df = selectBestK(df)
+    if mode=="V":
+        df = removeLowVariance(mode,df)
+    df = performL1(mode,df)
+    df = performRandomForest(mode,df)
+    if mode!="A":
+        df = selectBestK(mode,df)
     #print df
-    fileOP = os.path.join(config.SEL_FEAT,"nondiscriminative_linguistic_selected.csv")
+    output_file = ""
+    if qtype=="N":
+        output_file+="non"
+    output_file+="discriminative_"
+    if mode=="V":
+        output_file+="visual"
+    elif mode=="A":
+        output_file+="acoustic"
+    else:
+        output_file+="linguistic"
+    output_file+="_selected.csv"
+    fileOP = os.path.join(config.SEL_FEAT,output_file)
     df.to_csv(fileOP,sep=",",index=False)
 
 if __name__ == '__main__':
