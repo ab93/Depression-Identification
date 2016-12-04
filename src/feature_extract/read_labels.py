468ef2ce409ed4b2a756b9b0aef0d2fda13183a1
ab93 <avikbasu93@gmail.com>
2016-12-04 00:11:53 -0800

    Fixed indentation bug

--- src/feature_extract/read_labels.py
+++ src/feature_extract/read_labels.py
@@ -3,22 +3,22 @@ from sklearn import preprocessing
 import pprint
 
 def get_features(data,classifier_type="C"):
-    grouped = data.groupby('video')
-    X = []
-    y = []
     if classifier_type=="C":
         y_label = 'label'
     else:
         y_label = 'score'
+    grouped = data.groupby('video')
+    X = []
+    y = []
     for video, group in grouped:
         X_person = []
         y_person = []
         for i in range(len(group)):
-            X_person.append(group.iloc[i].tolist()[1:-1])
-            y_person.append(group.iloc[i][y_label])
+            X_person.append(group.iloc[i].tolist()[1:-2])
+            y_person.append(group.iloc[i]['label'])
         X.append(X_person)
         y.append(y_person)
-        return X,y
+    return X,y
 
 def features(mode,classifier,split,problem_type='C',normalize="regular"):
     if mode == "visual":
@@ -27,11 +27,5 @@ def features(mode,classifier,split,problem_type='C',normalize="regular"):
         file = "data/selected_features/"+normalize+"/"+split+"/"+classifier+"_acoustic_"+split+".csv"
     elif mode == "linguistic":
         file = "data/selected_features/"+normalize+"/"+split+"/"+classifier+"_linguistic_"+split+".csv"
-    print file
     data = pd.read_csv(file)
     return get_features(data,problem_type)
-
-features("visual","discriminative","train","R","regular")
-
-
-
