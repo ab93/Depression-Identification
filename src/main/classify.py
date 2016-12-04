61734f93d99b6358b8296c49659775d1e8b9c58c
ab93 <avikbasu93@gmail.com>
2016-12-04 00:12:53 -0800

    created utils, modified main func in classify

--- src/main/classify.py
+++ src/main/classify.py
@@ -15,69 +15,7 @@ from sklearn.pipeline import _name_estimators
 from ..feature_extract.read_labels import features
 import config
 import feature_select
-
-def oversample(X,y):
-    X = np.vstack(X)
-    y = np.hstack(y)
-    df = pd.DataFrame(X)
-    df['labels'] = y
-    grouped_df = df.groupby('labels')
-    for key, dframe in grouped_df:
-        if key == 1:
-            sampled_df = dframe
-    df = df.append(sampled_df)
-    data = df.values
-    X, y = data[:,:-1], data[:,-1]
-    return X, y
-
-
-def get_single_mode_data(mode='acoustic', category='PN', problem_type='C', normalize='regular'):
-    """
-    Get training and validation data for a single mode int
-    a particular category
-
-    Args:
-        mode: str
-        category: str
-    
-    Returns:
-        X_train: 3D numpy array
-        y_train: 2D numpy array
-        X_val: 3D numpy array
-        y_val: 2D numpy array
-    """
-    
-    if category == 'PN':
-        cat_1 = "positive"
-        cat_2 = "negative"
-    else:
-        cat_1 = "discriminative"
-        cat_2 = "nondiscriminative"
-
-    X_train = [map(np.asarray, features(mode,cat_1,"train", problem_type, normalize)[0]),
-            map(np.asarray, features(mode,cat_2,"train", problem_type, normalize)[0])]
-    y_train = [map(np.asarray,features(mode,cat_1,"train", problem_type, normalize)[1] ),
-            map(np.asarray, features(mode,cat_2,"train", problem_type, normalize)[1])]
-    X_val = [map(np.asarray, features(mode,cat_1,"val", problem_type, normalize)[0]),
-            map(np.asarray, features(mode,cat_2,"val", problem_type, normalize)[0])]
-    y_val = [map(np.asarray, features(mode,cat_1,"val", problem_type, normalize)[1]),
-            map(np.asarray, features(mode,cat_2,"val", problem_type, normalize)[1])]
-
-    return X_train, y_train, X_val, y_val
-
-
-def get_multi_data(category='PN'):
-    X_A_train, y_A_train, X_A_val, y_A_val = get_single_mode_data('acoustic', category)
-    X_V_train, y_V_train, X_V_val, y_V_val = get_single_mode_data('visual', category)
-    X_L_train, y_L_train, X_L_val, y_L_val = get_single_mode_data('linguistic', 'PN')
-
-    Xs = [X_A_train, X_V_train, X_L_train]
-    ys = [y_A_train, y_V_train, y_L_train]
-    Xs_val = [X_A_val, X_V_val, X_L_val]
-    ys_val = [y_A_val, y_V_val, y_L_val]
-
-    return Xs, ys, Xs_val, ys_val
-
+from utils import get_multi_data, get_single_mode_data
 
 def grid_search_meta(mode='acoustic',category='PN'):
     X_train, y_train, X_val, y_val = get_single_mode_data(mode, category)
@@ -92,16 +30,16 @@ def grid_search_meta(mode='acoustic',category='PN'):
     C_vals = np.logspace(-3,3,num=5)
     gammas = np.logspace(-3,3,num=5)
     penalties = ('l1','l2')
-    
+
 
     max_features_ = np.arange(3,20,5)
     max_depths = np.arange(3,6)
-    min_samples_leaves = np.arange(2,6)  
+    min_samples_leaves = np.arange(2,6)
 
     estimators = [LogisticRegression, DecisionTreeClassifier]
     named_clfs = _name_estimators(estimators)
     named_clfs = [x[0] for x in named_clfs]
-    dt_vals = [{'max_features': x, 'max_depth': y, 'min_samples_leaf': z} 
+    dt_vals = [{'max_features': x, 'max_depth': y, 'min_samples_leaf': z}
                 for x in max_features_ for y in max_depths for z in min_samples_leaves]
     svm_vals = [{'C':x, 'gamma':y} for x in C_vals for y in gammas]
     max_ent_vals = [{'C':x, 'penalty':y } for x in C_vals for y in penalties]
@@ -131,7 +69,7 @@ def grid_search_meta(mode='acoustic',category='PN'):
                 for clf_wt in clf_weights:
                     for class_wt in class_weights:
                         for C1 in C_vals:
-                            for C2 in C_vals: 
+                            for C2 in C_vals:
                                 clf_D = LogisticRegression(C=C1, penalty=p1, n_jobs=-1, class_weight={1:class_wt})
                                 clf_ND = LogisticRegression(C=C2, penalty=p2, n_jobs=-1, class_weight={1:class_wt})
                                 meta_clf = MetaClassifier(classifiers=[clf_D, clf_ND], weights=clf_wt)
@@ -139,13 +77,13 @@ def grid_search_meta(mode='acoustic',category='PN'):
                                 f1_score = meta_clf.score(X_val, y_true_val)
                                 if not clf_wt:
                                     clf_wt = [0.5, 0.5]
-                                outfile.write(str(clf_wt[0]) + ' ' + str(clf_wt[1]) + ',' + 
-                                            str(class_wt) + ',' + str(C1) + ',' + str(C2) + ',' 
+                                outfile.write(str(clf_wt[0]) + ' ' + str(clf_wt[1]) + ',' +
+                                            str(class_wt) + ',' + str(C1) + ',' + str(C2) + ','
                                             + p1 + ',' + p2 + ','
                                             + str(f1_score) + '\n')
                                 print f1_score
 
-    
+
 
 
 def grid_search_lf(category='PN'):
@@ -178,7 +116,7 @@ def grid_search_lf(category='PN'):
     clf_V = MetaClassifier(classifiers=[clf_V_1, clf_V_2])
     clf_L = MetaClassifier(classifiers=[clf_L_1, clf_L_2])
 
-    mode_weights = [None, [0.6, 0.3, 0.1], [0.3, 0.6, 0.1], [0.4, 0.4, 0.2], 
+    mode_weights = [None, [0.6, 0.3, 0.1], [0.3, 0.6, 0.1], [0.4, 0.4, 0.2],
                     [0.5, 0.4, 0.1], [0.4, 0.5, 0.1], [0.25, 0.25, 0.5]]
 
     with open(os.path.join(config.GRID_SEARCH_DIR, 'late_fusion_DND.csv'),'w') as outfile:
@@ -189,16 +127,21 @@ def grid_search_lf(category='PN'):
             f1_score = lf_clf.score(Xs_val,y_true_val,scoring='f1')
             if not mode_wt:
                 mode_wt = [0.3, 0.3, 0.3]
-            outfile.write(str(mode_wt[0]) + ',' + str(mode_wt[1]) + ',' + 
+            outfile.write(str(mode_wt[0]) + ',' + str(mode_wt[1]) + ',' +
                             str(mode_wt[2]) + ',' +str(f1_score) + '\n')
             print f1_score
 
 
 def main():
-    feature_select.feature_select("C")
+    #print "Selecting features...\n"
+    #feature_select.feature_select("C")
+    print "Performing Grid Search for visual...\n"
     grid_search_meta(mode='visual', category='PN')
+    print "Performing Grid Search for acoustic...\n"
     grid_search_meta(mode='acoustic', category='PN')
+    print "Performing Grid Search for linguistic...\n"
     grid_search_meta(mode='linguistic', category='PN')
+    print "Performing Grid Search for Late Fusion...\n"
     grid_search_lf(category='PN')
 
 
---
+++ src/main/utils.py
@@ -0,0 +1,68 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+from ..feature_extract.read_labels import features
+import config
+
+def oversample(X,y):
+    X = np.vstack(X)
+    y = np.hstack(y)
+    df = pd.DataFrame(X)
+    df['labels'] = y
+    grouped_df = df.groupby('labels')
+    for key, dframe in grouped_df:
+        if key == 1:
+            sampled_df = dframe
+    df = df.append(sampled_df)
+    data = df.values
+    X, y = data[:,:-1], data[:,-1]
+    return X, y
+
+
+def get_single_mode_data(mode='acoustic', category='PN', problem_type='C', normalize='regular'):
+    """
+    Get training and validation data for a single mode int
+    a particular category
+
+    Args:
+        mode: str
+        category: str
+
+    Returns:
+        X_train: 3D numpy array
+        y_train: 2D numpy array
+        X_val: 3D numpy array
+        y_val: 2D numpy array
+    """
+
+    if category == 'PN':
+        cat_1 = "positive"
+        cat_2 = "negative"
+    else:
+        cat_1 = "discriminative"
+        cat_2 = "nondiscriminative"
+    
+    X_train = [map(np.asarray, features(mode,cat_1,"train", problem_type, normalize)[0]),
+            map(np.asarray, features(mode,cat_2,"train", problem_type, normalize)[0])]
+    y_train = [map(np.asarray,features(mode,cat_1,"train", problem_type, normalize)[1] ),
+            map(np.asarray, features(mode,cat_2,"train", problem_type, normalize)[1])]
+    X_val = [map(np.asarray, features(mode,cat_1,"val", problem_type, normalize)[0]),
+            map(np.asarray, features(mode,cat_2,"val", problem_type, normalize)[0])]
+    y_val = [map(np.asarray, features(mode,cat_1,"val", problem_type, normalize)[1]),
+            map(np.asarray, features(mode,cat_2,"val", problem_type, normalize)[1])]
+
+    return X_train, y_train, X_val, y_val
+
+
+def get_multi_data(category='PN', problem_type='C', normalize='regular'):
+    X_A_train, y_A_train, X_A_val, y_A_val = get_single_mode_data('acoustic', category, problem_type, normalize)
+    X_V_train, y_V_train, X_V_val, y_V_val = get_single_mode_data('visual', category, problem_type, normalize)
+    X_L_train, y_L_train, X_L_val, y_L_val = get_single_mode_data('linguistic', 'PN', problem_type, normalize)
+
+    Xs = [X_A_train, X_V_train, X_L_train]
+    ys = [y_A_train, y_V_train, y_L_train]
+    Xs_val = [X_A_val, X_V_val, X_L_val]
+    ys_val = [y_A_val, y_V_val, y_L_val]
+
+    return Xs, ys, Xs_val, ys_val
