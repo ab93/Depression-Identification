0f98cda9a4a20b44fa9beeb8394c5bc975d84c73
ab93 <avikbasu93@gmail.com>
2016-12-04 01:46:33 -0800

    Refected changed dirs

--- src/main/config.py
+++ src/main/config.py
@@ -11,14 +11,5 @@ SEL_FEAT_TRAIN_NORMALIZED = os.path.join('data','selected_features','normalize',
 SEL_FEAT_VAL_NORMALIZED = os.path.join('data','selected_features','normalize','val')
 SEL_FEAT = os.path.join('data','selected_features')
 ANOVA_DIR = os.path.join('results','anova')
-GRID_SEARCH_DIR = os.path.join('results','grid_search')
-
-
-
-
-
-# TRAIN_SPLIT_FILE = 'data/classification_data/training_split.csv'
-# TEST_SPLIT_FILE = 'data/classification_data/test_split.csv'
-# VAL_SPLIT_FILE = 'data/classification_data/dev_split.csv'
-# D_ND_DIR = 'data/disc_nondisc'
-# POS_NEG_DIR = 'data/pos_neg'
+GRID_SEARCH_CLF_DIR = os.path.join('results','grid_search','classification')
+GRID_SEARCH_REG_DIR = os.path.join('results','grid_search','regression')
